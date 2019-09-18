7fbb644167a31d777f6c0963d0f2b90a4b002c33
AlecDWalker <walkeralec@hotmail.com>
2019-09-18 16:55:49 +0100

    capacity adjusted

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,17 +5,23 @@ describe DockingStation do
   it 'releases working bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+    expect( subject.release_bike ).to eq bike
   end
 
   it 'raises an error when there are no bikes available' do
-    expect {subject.release_bike}.to raise_error "No bikes available"
+    expect { subject.release_bike }.to raise_error "No bikes available"
   end
 
-  it 'docks a bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+
+  it 'raises and error when the station is full' do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error "Bike dock full"
   end
+
+  # it 'docks a bike' do
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #   expect(subject.bike).to eq bike
+  # end
 
 end
